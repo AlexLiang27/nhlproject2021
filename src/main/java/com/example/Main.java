a001488b4939f6cb92b7efa1f2d852f2246a954b
Andrew Siu <aks73@sfu.ca>
2021-07-08 19:10:20 -0700

    test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -54,58 +54,6 @@ public class Main {
     return "register";
   }
 
-<<<<<<< HEAD
-  @GetMapping("/register")
-  String cheese(){
-    return "register";
-  }
-
-  @GetMapping(path="/login")
-  String potato() {
-    return "login";
-  }
-
-  @GetMapping(path="/registeruser")
-  public String userRegister(Map<String, Object> model, User user) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users (id serial, username varchar(15), password varchar(15))");
-      ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-      while (rs.next()) {
-        if (user.getUserName() == rs.getString("username")) {
-          // redirect back to register page with error message (username is already taken, please choose another username)
-        }
-      }
-      String sql = "INSERT INTO users (username, password) VALUES ('" + user.getUserName() + "','" + user.getPassword() + "')";
-      stmt.executeUpdate(sql);
-      return "error";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
-
-  @GetMapping(path="/loginuser")
-  public String userLogin(Map<String, Object> model, User user) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-      while (rs.next()) {
-        if (user.getUserName() == rs.getString("username") && user.getPassword() == rs.getString("password")) { // check if works later
-          return "redirect:/home";
-        }
-        else {
-          // redirect back to login page with error message (username or password is incorrect)
-          return "error";
-        }
-      }
-      return "error";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
-=======
   // @GetMapping("/registeruser")
   // String userRegister(Map<String, Object> model, User user) {
   //   try (Connection connection = dataSource.getConnection()) {
@@ -144,7 +92,6 @@ public class Main {
   //     return "error";
   //   }
   // }
->>>>>>> 61a03f6a8284473bb106042cdd1940e7ba077fe2
 
   @Bean
   public DataSource dataSource() throws SQLException {
--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,5 +1,4 @@
 package com.example;
-<<<<<<< HEAD
 public class User {
     private String user;
     private String pass;
@@ -15,35 +14,5 @@ public class User {
     }
     public void setPassword(String p){
         this.pass=p;
-=======
-
-public class User {
-    private int id;
-    private String username;
-    private String password;
-
-    public int getID() {
-        return this.id;
-    }
-
-    public String getUserName() {
-        return this.username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setID(int id) {
-        this.id = id;
-    }
-
-    public void setUserName(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
->>>>>>> 61a03f6a8284473bb106042cdd1940e7ba077fe2
     }
 }
