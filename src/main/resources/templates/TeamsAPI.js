51bd2e52d923a5fe8965c0719c38ebca64fe105c
Manav Acharya <manav_acharya@sfu.ca>
2021-07-20 16:39:38 -0700

    API Access Success

---
+++ src/main/resources/templates/TeamsAPI.js
@@ -0,0 +1,9 @@
+
+
+$.getJSON("https://statsapi.web.nhl.com/api/v1/teams", function(data) {
+    console.log(data);
+
+    var teamname = data.teams[0].name;
+
+    $(".teamname").append(teamname);
+});
