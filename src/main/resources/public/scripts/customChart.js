2e5a92828364f5c558a9a83e7d668ea0ce049686
Andrew Siu <aks73@sfu.ca>
2021-08-03 18:07:32 -0700

    added comparison skeleton

---
+++ src/main/resources/public/scripts/customChart.js
@@ -0,0 +1,30 @@
+var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+
+var chBar = document.getElementById("chBar");
+if (chBar) {
+    new Chart(chBar, {
+    type: 'bar',
+    data: {
+    labels: ["assists", "goals", "pim", "shots", "games", "powerPlayGoals", "powerPlayPoints", "penaltyMinutes", "shotPct", "gameWinningGoals", "overTimeGoals", "shortHandedGoals", "shortHandedPoints", "plusMinus", "points"],
+    datasets: [{
+        data: [589, 445, 483, 503, 689, 692, 634, 123, 654, 789, 852, 963, 741, 159, 357],
+        backgroundColor: colors[0]
+    },
+    {
+        data: [639, 465, 493, 478, 589, 632, 674, 357, 159, 741, 963, 852, 789, 654, 123],
+        backgroundColor: colors[1]
+    }]
+    },
+    options: {
+    legend: {
+        display: false
+    },
+    scales: {
+        xAxes: [{
+        barPercentage: 0.4,
+        categoryPercentage: 0.5
+        }]
+    }
+    }
+    });
+}
